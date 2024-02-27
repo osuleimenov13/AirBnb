c41c99b11a3eb24ff1ff6177533e378e43fe2909
Olzhas Suleimenov <olzhassuleimenov@Olzhass-MacBook-Air.local>
2024-02-27 22:59:41 +0600

    Creating SearchAndFilterBar

---
+++ AirBnb/Core/Explore/SearchAndFilterBar.swift
@@ -0,0 +1,49 @@
+//
+//  SearchAndFilterBar.swift
+//  AirBnb
+//
+//  Created by Olzhas Suleimenov on 27.02.2024.
+//
+
+import SwiftUI
+
+struct SearchAndFilterBar: View {
+    var body: some View {
+        HStack {
+            Image(systemName: "magnifyingglass")
+            
+            VStack(alignment: .leading, spacing: 2) {
+                Text ("Where to?")
+                    .font(.footnote)
+                    .fontWeight(.semibold)
+                Text("Anywhere - Any Week - Add guests")
+                    .font(.caption2)
+                    .foregroundStyle(.gray)
+            }
+            
+            Spacer()
+            
+            Button {
+                
+            } label: {
+                Image(systemName: "line.3.horizontal.decrease.circle")
+                    .foregroundStyle(.black)
+            }
+        }
+        .padding(.horizontal)
+        .padding(.vertical, 10)
+        .overlay {
+            Capsule()
+                .stroke(lineWidth: 0.5)
+                .foregroundStyle(Color(.systemGray4))
+                .shadow(color: .black.opacity(0.4), radius: 2)
+        }
+        .padding()
+    }
+}
+
+struct SearchAndFilterBar_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchAndFilterBar()
+    }
+}
